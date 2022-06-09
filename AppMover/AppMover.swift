b25b041df4fea47723b5c57fdb2b6fbffbcbc8df
rob <me@iamcalledrob.com>
2022-06-09 10:42:47 -0700

    Refactor and lifecycle improvements

--- AppMover/AppMover.swift
+++ AppMover/AppMover.swift
@@ -3,123 +3,330 @@
 //  AppMover
 //
 //  Created by Oskar Groth on 2019-12-20.
-//  Copyright © 2019 Oskar Groth. All rights reserved.
-//
+//  Forked and updated by Rob Mason in 2022.
 
 import AppKit
-import Security
 
-public enum AppMover {
-        
+public struct AppMover {
+    
+    public static var isInstalled: Bool {
+        return isInsideApplicationsFolder(url: Bundle.main.bundleURL)
+    }
+    
+    /// Backwards-compatible non-throwing implementation of `moveApp`.
     public static func moveIfNecessary() {
-        let fm = FileManager.default
-        guard !Bundle.main.isInstalled,
-            let applications = preferredInstallDirectory() else { return }
-        let bundleUrl = Bundle.main.bundleURL
-        let bundleName = bundleUrl.lastPathComponent
-        let destinationUrl = applications.appendingPathComponent(bundleName)
-        let needDestAuth = fm.fileExists(atPath: destinationUrl.path) && !fm.isWritableFile(atPath: destinationUrl.path)
-        let needAuth = needDestAuth || !fm.isWritableFile(atPath: applications.path)
+        do {
+            try moveApp(installedName: .current, replaceNewerVersions: false)
+        } catch {
+            NSLog("Moving app: \(error)")
+        }
+    }
+    
+    /// Moves the running app bundle into an appropriate "Applications" Folder if necessary, prompting the user first.
+    ///
+    /// - Parameters:
+    ///    - installedName: Specifies the name the app bundle should take when copied.
+    ///    - stringBuilder: Allows for customising of prompt strings, passing in whether admin authentication is needed for the install.
+    ///    - replaceNewerVersions: Specifies whether the current app should replace a newer version that's already installed.
+    ///      If `false`, this app will be killed and the newer version launched instead. This can be useful to correct accidental launches of
+    ///      older versions from the Downloads folder, for example. The `CFBundleShortVersionString` is used for comparison.
+    ///
+    /// If a move is necessary, this function will block until operations are complete.
+    ///
+    /// If the move is successful, this process will be killed and the function will therefore never return.
+    /// If the move fails, an error of type `AppMoverError` will be thrown.
+    public static func moveApp(
+        installedName: AppName = .CFBundleName,
+        stringBuilder: (_ needsAuth: Bool) -> Strings = Strings.standard,
+        replaceNewerVersions: Bool = false
+    ) throws {
+        guard !isInsideApplicationsFolder(url: Bundle.main.bundleURL) else {
+            return
+        }
         
         // Activate app -- work-around for focus issues related to "scary file from
         // internet" OS dialog.
-        if !NSApp.isActive {
-            NSApp.activate(ignoringOtherApps: true)
+        NSApp.activate(ignoringOtherApps: true)
+        
+        guard let applicationsDirectoryUrl = preferredApplicationsDirectory() else {
+            throw AppMoverError(info: "Applications directory could not be located")
+        }
+        let destinationUrl = applicationsDirectoryUrl.appendingPathComponent(installedName.string + ".app")
+        
+        // The following two checks are useful when the app has been re-launched from the Downloads folder after
+        // previously being installed.
+        
+        // App at install destination is already running. Switch to that app and terminate this instance.
+        // Killing another running process wouldn't be a good idea anyway.
+        guard !isApplicationRunning(url: destinationUrl) else {
+            NSLog("Fatal: App already running at \(destinationUrl). Switching to app then killing this process.")
+            NSWorkspace.shared.open(destinationUrl)
+            exit(0)
         }
         
-        let alert = NSAlert()
-        alert.messageText = "Move to Applications folder"
-        alert.informativeText = "\(Bundle.main.localizedName) needs to move to your Applications folder in order to work properly."
-        if needAuth {
-            alert.informativeText.append(" You need to authenticate with your administrator password to complete this step.")
+        // App at install destination is newer than this version
+        guard replaceNewerVersions == true || !isNewerApplicationInstalled(url: destinationUrl) else {
+            NSLog("Fatal: Newer app version installed at \(destinationUrl). Switching to app then killing this process.")
+            NSWorkspace.shared.open(destinationUrl)
+            exit(0)
         }
-        alert.addButton(withTitle: "Move to Applications Folder")
-        alert.addButton(withTitle: "Do Not Move")
+        
+        let needsAuth = needsAuth(toWriteTo: destinationUrl) || needsAuth(toWriteTo: applicationsDirectoryUrl)
+        
+        let strings = stringBuilder(needsAuth)
+        let alert = NSAlert()
+        alert.messageText = strings.title
+        alert.informativeText = strings.body
+        alert.addButton(withTitle: strings.moveButton)
+        alert.addButton(withTitle: strings.cancelButton)
+        
+        // Cancel pressed
         guard alert.runModal() == .alertFirstButtonReturn else {
             return
         }
-        if needAuth {
-            let result = authorizedInstall(from: bundleUrl, to: destinationUrl)
-            guard !result.cancelled else { moveIfNecessary(); return }
-            guard result.success else {
-                NSApplication.shared.terminate(self)
-                return
-            }
+        
+        if !needsAuth {
+            try install(to: destinationUrl)
         } else {
-            if fm.fileExists(atPath: destinationUrl.path) {
-                if AppMover.isApplicationAtUrlRunning(destinationUrl) {
-                    NSWorkspace.shared.open(destinationUrl)
-                    return
-                } else {
-                    guard (try? fm.trashItem(at: destinationUrl, resultingItemURL: nil)) != nil else {
-                        return
-                    }
-                }
-            }
-            guard (try? fm.copyItem(at: bundleUrl, to: destinationUrl)) != nil else {
+            let result = authenticatedInstall(to: destinationUrl)
+            switch result {
+            case .error(let number, let message):
+                throw AppMoverError(info: "Authenticated install failed: number=\(number), message=\(message)")
+            case .cancelled:
+                // Catches "cancel" presses on the auth prompt.
+                //
+                // Instead of cancelling the whole app move, this will have the effect of returning the user
+                // back to the first dialog.
+                //
+                // Useful if the "cancel" was a mis-click.
+                try moveApp(
+                    installedName: installedName,
+                    stringBuilder: stringBuilder,
+                    replaceNewerVersions: replaceNewerVersions)
                 return
+            case .success:
+                break
             }
         }
         
-        // Trash the original app
-        _ = try? fm.removeItem(at: bundleUrl)
-        
-        relaunch(at: destinationUrl.path, completionCallback: {
-            DispatchQueue.main.async {
-                exit(0)
-            }
-        })
-        
-    }
-    
-    static func authorizedInstall(from sourceURL: URL, to destinationURL: URL) -> (cancelled: Bool, success: Bool) {
-        guard destinationURL.representsBundle,
-            destinationURL.isValid,
-            sourceURL.isValid else {
-            return (false, false)
+        // Catch this one: trashing the current bundle is not strictly necessary
+        do {
+            try FileManager.default.trashItem(at: Bundle.main.bundleURL, resultingItemURL: nil)
+        } catch {
+            NSLog("Trashing \(destinationUrl) failed: \(error)")
         }
-        return sourceURL.withUnsafeFileSystemRepresentation({ sourcePath -> (cancelled: Bool, success: Bool) in
-            return destinationURL.withUnsafeFileSystemRepresentation({ destinationPath -> (cancelled: Bool, success: Bool) in
-                guard let sourcePath = sourcePath, let destinationPath = destinationPath else { return (false, false) }
-                let deleteCommand = "rm -rf '\(String(cString: destinationPath))'"
-                let copyCommand = "cp -pR '\(String(cString: sourcePath))' '\(String(cString: destinationPath))'"
-                guard let script = NSAppleScript(source: "do shell script \"\(deleteCommand) && \(copyCommand)\" with administrator privileges") else {
-                    return (false, false)
-                }
-                var error: NSDictionary?
-                script.executeAndReturnError(&error)
-                return ((error?[NSAppleScript.errorNumber] as? Int16) == -128, error == nil)
-            })
-        })
+        
+        // Spawn a separate process to launch the new app once this process has been killed
+        try launchAfterKilled(applicationAt: destinationUrl)
+        
+        // Kill this process.
+        exit(0)
     }
     
-    static func preferredInstallDirectory() -> URL? {
-        let fm = FileManager.default
-        let dirs = fm.urls(for: .applicationDirectory, in: .allDomainsMask)
-        // Find Applications dir with the most apps that isn't system protected
-        return dirs.map({ $0.resolvingSymlinksInPath() }).filter({ url in
-            var isDir: ObjCBool = false
-            fm.fileExists(atPath: url.path, isDirectory: &isDir)
-            return isDir.boolValue && url.path != "/System/Applications"
-        }).sorted(by: { left, right in
-            return left.numberOfFilesInDirectory < right.numberOfFilesInDirectory
-        }).last
+    // MARK: - Lifecycle
+    
+    private static func install(to url: URL) throws {
+        if FileManager.default.fileExists(atPath: url.path) {
+            try FileManager.default.trashItem(at: url, resultingItemURL: nil)
+        }
+        try FileManager.default.copyItem(at: Bundle.main.bundleURL, to: url)
+        try unquarantine(url: url)
     }
     
-    static func isApplicationAtUrlRunning(_ url: URL) -> Bool {
-        let url = url.standardized
-        return NSWorkspace.shared.runningApplications.contains(where: {
-            $0.bundleURL?.standardized == url
-        })
+    private enum AuthenticatedInstallResult {
+        case success
+        case error(number: Int16, message: String)
+        case cancelled
     }
     
-    public static func relaunch(at path: String, completionCallback: @escaping () -> Void) {
+    private static func authenticatedInstall(to url: URL) -> AuthenticatedInstallResult {
+        // The following code needs to be treated carefully.
+        // It will be run with admin privileges and so malicious intent should be considered.
+        let source = Bundle.main.bundleURL.absoluteURL.path
+        let dest = url.absoluteURL.path
+        
+        let deleteCommand = "/bin/rm -rf -- '\(dest)'"
+        let copyCommand = "/bin/cp -pR -- '\(source)' '\(dest)'"
+        let quarantineCommand = "/usr/bin/xattr -d -r com.apple.quarantine '\(dest)'"
+        let script = "do shell script \"\(deleteCommand) && \(copyCommand) && \(quarantineCommand)\" with administrator privileges"
+        
+        guard let script = NSAppleScript(source: script) else {
+            fatalError("Unable to parse AppleScript source: \(script)")
+        }
+        
+        var error: NSDictionary?
+        script.executeAndReturnError(&error)
+        
+        // Success
+        guard let error = error else {
+            return .success
+        }
+        
+        // Cancel pressed
+        if (error[NSAppleScript.errorNumber] as? Int16) == -128 {
+            return .cancelled
+        }
+        
+        // Some other error
+        return .error(
+            number:  error[NSAppleScript.errorNumber] as? Int16 ?? 0,
+            message: error[NSAppleScript.errorMessage] as? String ?? ""
+        )
+    }
+    
+    private static func unquarantine(url: URL) throws {
+        let task = Process()
+        task.launchPath = "/usr/bin/xattr"
+        task.arguments = ["-d", "-r", "com.apple.quarantine", url.absoluteURL.path]
+        try task.run()
+        task.waitUntilExit()
+    }
+
+    /// Spawns a new process that will launch the app at `url` once `kill -0` reports the current pid is no longer running.
+    private static func launchAfterKilled(applicationAt url: URL) throws {
         let pid = ProcessInfo.processInfo.processIdentifier
-        Process.runTask(command: "/usr/bin/xattr", arguments: ["-d", "-r", "com.apple.quarantine", path], completion: { _ in
-            let waitForExitScript = "(while /bin/kill -0 \(pid) >&/dev/null; do /bin/sleep 0.1; done; /usr/bin/open \"\(path)\") &"
-            Process.runTask(command: "/bin/sh", arguments: ["-c", waitForExitScript])
-            completionCallback()
+        let task = Process()
+        task.launchPath = "/bin/sh"
+        let script = "(while /bin/kill -0 \(pid) >&/dev/null; do /bin/sleep 0.1; done; /usr/bin/open \"\(url.absoluteURL.path)\") &"
+        task.arguments = ["-c", script]
+        try task.run()
+    }
+    
+    // MARK: - Helpers
+    
+    static func needsAuth(toWriteTo url: URL) -> Bool {
+        return FileManager.default.fileExists(atPath: url.path) &&
+            !FileManager.default.isWritableFile(atPath: url.path)
+    }
+
+    static func isApplicationRunning(url: URL) -> Bool {
+        return NSWorkspace.shared.runningApplications.contains(where: {
+            $0.bundleURL?.standardized == url.standardized
         })
     }
     
+    static func isNewerApplicationInstalled(url: URL) -> Bool {
+        guard FileManager.default.fileExists(atPath: url.path) else {
+            NSLog("No file exists at \(url.path)")
+            return false
+        }
+        
+        guard let installedVersion = MDItemCopyAttribute(MDItemCreateWithURL(kCFAllocatorDefault, url as CFURL), kMDItemVersion) as? String else {
+            NSLog("Failed to retrieve kMDItemVersion for \(url)")
+            return false
+        }
+        
+        guard let thisVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            NSLog("Failed to retrieve CFBundleShortVersionString from bundle")
+            return false
+        }
+                        
+        // "The left operand is greater than the right operand."
+        // = installedVersion > thisVersion
+        return cleanedBundleVersion(installedVersion)
+            .compare(cleanedBundleVersion(thisVersion), options: .numeric) == .orderedDescending
+    }
+    
+    // Converts 1.0 -> 1.0.0 etc to workaround issue where `compare(_,options: .numeric)` will report
+    // incorrectly that 1.0 > 1.0.0
+    static func cleanedBundleVersion(_ version: String) -> String {
+        var parts = version.split(separator: ".")
+        for _ in parts.count..<3 {
+            parts.append("0")
+        }
+        return parts.joined(separator: ".")
+    }
+
+    static func numberOfFilesInDirectory(url: URL) -> Int {
+        (try? FileManager.default.contentsOfDirectory(atPath: url.path))?.count ?? 0
+    }
+
+    static func preferredApplicationsDirectory() -> URL? {
+        let candidates = FileManager.default.urls(for: .applicationDirectory, in: .allDomainsMask)
+        
+        // Find Applications dir with the most apps that isn't system protected
+        return candidates
+            .map({ $0.resolvingSymlinksInPath() })
+            .filter({ url in
+                var isDir: ObjCBool = false
+                FileManager.default.fileExists(atPath: url.path, isDirectory: &isDir)
+                return isDir.boolValue && url.path != "/System/Applications"
+            })
+            .sorted(by: { lhs, rhs in
+                return numberOfFilesInDirectory(url: lhs) < numberOfFilesInDirectory(url: rhs)
+            })
+            .last
+    }
+
+    static func isInsideApplicationsFolder(url: URL) -> Bool {
+        let applicationDirs = NSSearchPathForDirectoriesInDomains(.applicationDirectory, .allDomainsMask, true)
+        if applicationDirs.contains(where: { url.absoluteURL.path.hasPrefix($0) }) {
+            return true
+        }
+        
+        if url.pathComponents.contains("Applications") {
+            return true
+        }
+        
+        return false
+    }
+
+}
+
+// MARK: - Configuration
+
+public enum AppName {
+    /// CFBundleName from Info.plist
+    /// This can be useful to prevent propogation of suffixes added by Archive Utility, e.g. "MyApp-1.app"
+    case CFBundleName
+    /// Name of currently running .app in its current location on disk, e.g. "MyApp.app" or "MyApp-1.app"
+    case current
+    /// Arbitrary name (excluding ".app" suffix)
+    case custom(String)
+    
+    var string: String {
+        switch self {
+        case .CFBundleName:
+            if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String {
+                return name
+            }
+            fallthrough
+        case .current:
+            return (Bundle.main.bundleURL.lastPathComponent as NSString).deletingPathExtension
+        case .custom(let name):
+            return name
+        }
+    }
+}
+
+public struct Strings {
+    let title: String
+    let body: String
+    let moveButton: String
+    let cancelButton: String
+    
+    public init(title: String, body: String, moveButton: String, cancelButton: String) {
+        self.title = title
+        self.body = body
+        self.moveButton = moveButton
+        self.cancelButton = cancelButton
+    }
+    
+    public static func standard(_ needsAuth: Bool) -> Strings {
+        var body = "\(NSRunningApplication.current.localizedName ?? "The App") needs to move to your Applications folder in order to work properly."
+        if needsAuth {
+            body += " You need to authenticate with your administrator password to complete this step."
+        }
+        return Strings(
+            title: "Move to Applications folder",
+            body: body,
+            moveButton: "Move to Applications Folder",
+            cancelButton: "Don't Move"
+        )
+    }
+}
+
+
+public struct AppMoverError: Error {
+    let info: String
 }
